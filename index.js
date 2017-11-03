0902e08e2ae6dd4b5feafa44b893460c928126b9
Owyn <Owyn@users.noreply.github.com>
2017-11-03 23:19:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-module.exports = function Lockons(dispatch) {
-  dispatch.hook('cCanLockonTarget', function(event) {
-    dispatch.toClient('sCanLockonTarget', Object.assign({ ok: true }, event));
-  });
+module.exports = function Lockons_light(dispatch) {
+	
+	let cache = [];
+	let cache_size = 60;
+	
+	dispatch.hook('C_CAN_LOCKON_TARGET', function(event) {
+		if(cache.includes(event.target))
+		{
+			dispatch.toClient('S_CAN_LOCKON_TARGET', Object.assign({ ok: true }, event));
+			console.log("using cached lockOn state" +event.target + " and unk: " + event.unk);
+		}
+	});
+	
+	dispatch.hook('S_CAN_LOCKON_TARGET', function(event) {
+		if(!cache.includes(event.target))
+		{
+			if(event.ok)
+			{
+				if(cache.length >= cache_size)
+				{
+					cache.pop();
+					console.log("cache max size reached");
+				}
+				cache.unshift(event.target);
+				console.log("adding new element to cache: " +event.target + " and unk: " + event.unk);
+			}
+		}
+		else if(!event.ok) // cached object became untargetable
+		{
+			cache.splice(cache.indexOf(event.target), 1);
+			console.log("cached element became untargetable" +event.target + " and unk: " + event.unk);
+		}
+	});
 };
